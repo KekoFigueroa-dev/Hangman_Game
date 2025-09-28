21aafafe15b4302380c04317579c44704fb9de99
KekoFigueroa-dev <thenaturalistgospel@gmail.com>
2025-09-28 01:16:29 -0600

    added free man

--- hangman.py
+++ hangman.py
@@ -1,7 +1,9 @@
-# Hangman Game
+# Hangman Game - Guess the word before the hangman is complete
+
 import random
 
-hangman_images =[
+# Hangman images for each incorrect guess
+hangman_images = [
     '''
   +---+
   |   |
@@ -53,63 +55,79 @@ hangman_images =[
       |
 =========''']
 
-#Print welcome message
+# Welcome message
 print("Welcome to my Hangman Game!")
-# Set up a list of possible words
-word_list = ["python", "hangman", "challenge", "programming", "developer", "function", "variable", "iteration", "condition", "loop"]
+
+# Expanded word list for more variety
+word_list = [
+    "python", "hangman", "challenge", "programming", "developer", "function", "variable", "iteration", "condition", "loop",
+    "algorithm", "syntax", "exception", "inheritance", "object", "class", "method", "parameter", "argument", "recursion",
+    "dictionary", "string", "integer", "boolean", "float", "list", "tuple", "set", "module", "package", "import",
+    "lambda", "comprehension", "debugging", "statement", "operator", "expression", "index", "slice", "mutable", "immutable"
+]
+
 is_running = True
-#Randomly select a word for the player to guess
-chosen_word = random.choice(word_list)
+
 while is_running:
+    # Select a random word and initialize game state
     correct_answer = list(random.choice(word_list))
-    correct_letters = []
+    correct_letters = ["_"] * len(correct_answer)
     guessed_letters = []
-    for letter in correct_answer:
-        correct_letters.append("_")
-    #Debug
-    print(f"Correct Answer: {correct_answer}")
-    print(f"Current Correct Letters: {correct_letters}")
-    input()
+
+    # Uncomment for debugging:
+    # print(f"Correct Answer: {''.join(correct_answer)}")
 
     guess_count = 0
     is_playing = True
-    while is_playing:
-        print(hangman_images[guess_count])
-        print(correct_letters)
-        print(f"You have guessed the following letters: {guessed_letters}")
 
-        #Get a guess
+    while is_playing:
+        # Show hangman image and current word progress
+        print(hangman_images[guess_count])
+        print(" ".join(correct_letters))
+        print(f"Guessed letters: {', '.join(guessed_letters)}")
+
+        # Get a valid letter guess from user
         guessed_letter = input("\nGuess a letter: ").lower()
-        while guessed_letter in guessed_letters:
-            print("You've already guessed that letter!")
+        while not (guessed_letter.isalpha() and len(guessed_letter) == 1) or guessed_letter in guessed_letters:
+            if guessed_letter in guessed_letters:
+                print("You've already guessed that letter!")
+            else:
+                print("Please enter a single alphabetic character.")
             guessed_letter = input("Guess a letter: ").lower()
         guessed_letters.append(guessed_letter)
 
-        #Correct guess!
+        # Update state if guess is correct
         if guessed_letter in correct_answer:
             print(f"\nCorrect! The letter {guessed_letter} is in the word!")
             for i in range(len(correct_answer)):
                 if guessed_letter == correct_answer[i]:
                     correct_letters[i] = guessed_letter
-        #Wrong guess!
+        # Update state if guess is incorrect
         else:
-            print(f"\nOuch!  The letter {guessed_letter} is not in the word!")
+            print(f"\nOuch! The letter {guessed_letter} is not in the word!")
             guess_count += 1
 
-        #Win or Lose Conditions
+        # Win condition: all letters guessed
         if correct_letters == correct_answer:
-            print("Congratulations!  You win!")
-            print(f"The word was {correct_answer}")
+            print("Congratulations! You win!")
+            print(f"The word was {''.join(correct_answer)}")
+            print(r"""
+      \O/
+       |
+      / \
+   The hangman walks free!
+    """)
             is_playing = False
+        # Lose condition: hangman complete
         elif guess_count == len(hangman_images) - 1:
             print(hangman_images[guess_count])
             print("YOU LOSE!")
-            print(f"The word was {correct_answer}")
+            print(f"The word was {''.join(correct_answer)}")
             is_playing = False
 
-    #Run again?
+    # Prompt to play again
     choice = input("\nWould you like to run the program again (y/n): ").lower()
     if choice.startswith("n"):
         is_running = False
 
-print("\nThank you for playing HANGMAN.")    
+print("\nThank you for playing HANGMAN.")
